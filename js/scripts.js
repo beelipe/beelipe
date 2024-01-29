f953e58686f2ea1e2f613eebeee24d270e7efc19
Reto <57033791+beelipe@users.noreply.github.com>
2024-01-29 21:49:24 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+// Get the modal
+var modal = document.getElementById("thankYou");
+
+// Get the button that opens the modal
+var btn = document.getElementById("sendButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeButton")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const elements = document.querySelectorAll('.viewport');
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: .4
+}
+const callbacks = (entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting){
+        entry.target.classList.add('active');
+    }
+  });
+}
+let observer = new IntersectionObserver(callbacks, options);
+elements.forEach(element => {
+  observer.observe(element);
+});
